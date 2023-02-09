b8e2c5cebe6c9a13b4576cae1bef20551a8d6690
jankrejci <krejci.cze@gmail.com>
2023-02-09 18:54:41 +0100

    lazygit: Add version detection check

--- lazygit/install.sh
+++ lazygit/install.sh
@@ -2,20 +2,31 @@
 
 CONFIG_FOLDER="$HOME/.config/lazygit"
 
+TARGET="x86_64"
 TMP_FOLDER="/tmp/lazygit"
 
-GITHUP_REPO="jesseduffield/lazygit"
-GITHUB_API="https://api.github.com/repos/$GITHUP_REPO/releases/latest"
-GITHUB_RELEASE="https://github.com/$GITHUB_REPO/releases/download/latest"
-
-VERSION=$(curl -s "$GITHUB_API" | grep -Po '"tag_name": "v\K[^"]*')
-TARGET="x86_64"
-SOURCE_FILE="lazygit_"$VERSION"_Linux_"$TARGET".tar.gz"
-SOURCE_LINK="$GITHUB_RELEASE/$SOURCE_FILE"
+GITHUB_REPO="jesseduffield/lazygit"
+GITHUB_API="https://api.github.com/repos/$GITHUB_REPO/releases/latest"
 
 echo "Lazygit installation"
 
-echo "    • installing version $VERSION from precompiled binary system wide"
+# Detect the latest version
+VERSION=$(curl -s "$GITHUB_API" | grep -Po '"tag_name": "v\K[^"]*')
+if [ -n "${VERSION}" ]; then
+  echo "    • version $VERSION"
+else
+  # Red text color
+  echo -en "\033[0;31m"
+  echo "    • failed to detect latest version"
+  echo -en "\033[0m"
+  exit 1
+fi
+
+GITHUB_RELEASE="https://github.com/$GITHUB_REPO/releases/download/v$VERSION"
+SOURCE_FILE="lazygit_"$VERSION"_Linux_"$TARGET".tar.gz"
+SOURCE_LINK="$GITHUB_RELEASE/$SOURCE_FILE"
+
+echo "    • installing from precompiled binary system wide"
 mkdir --parents "$TMP_FOLDER"
 wget -q "$SOURCE_LINK" -P "$TMP_FOLDER"
 tar -xf "$TMP_FOLDER/$SOURCE_FILE" -C "$TMP_FOLDER"
