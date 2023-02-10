e86742b459a09b480e639b4ff68de2f8c13411fc
Jan Krejčí <jan.krejci@braiins.cz>
2023-02-10 18:25:17 +0100

    zellij: Add support for installation from source

--- zellij/install.sh
+++ zellij/install.sh
@@ -1,19 +1,63 @@
 #!/bin/bash
 
 CONFIG_FOLDER="$HOME/.config/zellij"
+BUILD_FROM_SOURCE=false
+
+print_help(){
+    echo "Nothing to be seen here"
+}
+
+process_args() {
+  eval set -- "$VALID_ARGS"
+  while [ : ]; do
+    case "$1" in
+      -s | --source)
+          BUILD_FROM_SOURCE=true
+          shift
+          ;;
+      -h | --help)
+          print_help
+          shift
+          ;;
+      --)
+          shift; 
+          break 
+          ;;
+    esac
+  done
+}
+
+install_dependencies() {
+  echo "    • installing Ubuntu dependencies"
+  sudo apt -y install fonts-powerline &> /dev/null
+}
+
+install_from_binary() {
+  echo "    • installing from precompiled binary"
+  cargo binstall -y zellij &> /dev/null
+}
+
+install_from_source() {
+  echo "    • compiling from source, it may take a while"
+  cargo install --quiet --locked zellij
+}
+
+VALID_ARGS=$(getopt -o sh --long source,help -- "$@")
+if [[ $? -ne 0 ]]; then
+    exit 1;
+fi
+
+process_args
 
 echo "Zellij installation"
 
-echo "    • installing dependencies for Ubuntu"
-sudo apt -y install \
-fonts-powerline \
-&> /dev/null
+install_dependencies
 
-# echo "    • compiling from source, it may take a while"
-# cargo install --quiet --locked zellij
-
-echo "    • installing from precompiled binary"
-cargo binstall -y zellij &> /dev/null
+if [ $BUILD_FROM_SOURCE == true ]; then
+  install_from_source
+else
+  install_from_binary
+fi
 
 echo "    • linking configuration files"
 mkdir --parents $CONFIG_FOLDER
