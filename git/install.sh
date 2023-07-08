f7ff8e0133376aeff7278c2d78254fea80d75779
Jan Krejčí <krejci.cze@gmail.com>
2023-07-08 11:58:13 +0200

    Update the Git installation script

--- git/install.sh
+++ git/install.sh
@@ -1,8 +1,72 @@
-#!/bin/bash
+#!/usr/bin/env bash
 
-CONFIG_FOLDER="$HOME/.config/git"
+set -Eeuo pipefail
 
-echo "Git installation"
+trap cleanup SIGINT SIGTERM ERR EXIT
+
+cleanup() {
+	trap - SIGINT SIGTERM ERR EXIT
+	tput cnorm
+	# Script cleanup here
+}
+
+script_dir=$(cd "$(dirname "${BASH_SOURCE[0]}")" &>/dev/null && pwd -P)
+
+die() {
+	local msg=$1
+	local code=${2-1}
+	msg "$msg"
+	exit "$code"
+}
+
+setup_colors() {
+	if [[ -t 2 ]] && [[ -z "${NO_COLOR-}" ]] && [[ "${TERM-}" != "dumb" ]]; then
+		NOFORMAT='\033[0m' BOLD='\033[1m'
+	else
+		NOFORMAT='' BOLD=''
+	fi
+}
+
+msg() {
+	echo >&2 -e "$@"
+}
+
+usage() {
+	cat <<EOF
+Usage: $(basename "${BASH_SOURCE[0]}")
+
+Batch install oo the environment.
+
+Available options:
+
+-h, --help      Print this help and exit
+    --debug     Print script debug info
+    --no-color  Print without colors
+EOF
+	exit
+}
+
+parse_params() {
+    CONFIG_FOLDER="$HOME/.config/git"
+
+	while :; do
+		case "${1-}" in
+		-h | --help) usage ;;
+		--debug) set -x ;;
+		--no-color) NO_COLOR=1 ;;
+		-?*) die "Unknown option: $1" ;;
+		*) break ;;
+		esac
+		shift
+	done
+	return 0
+}
+
+sudo echo -n
+parse_params "$@"
+setup_colors
+
+msg "${BOLD}Git installation${NOFORMAT}"
 
 echo "    • installing from Ubuntu package"
 sudo apt -y install git &>/dev/null
