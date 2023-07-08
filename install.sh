9e3b7f616b1490fe53af690d44318cb0952baf84
Jan Krejčí <krejci.cze@gmail.com>
2023-07-08 14:15:54 +0200

    Install fontconfig for nerd font configuration

--- install.sh
+++ install.sh
@@ -60,11 +60,28 @@ parse_params() {
 	return 0
 }
 
+install_dependencies() {
+	read -r -a packages <<<"$@"
+	for package in "${packages[@]}"; do
+		if ! dpkg -s "$package" &>/dev/null; then
+			not_installed_packages+=("$package")
+		fi
+	done
+
+	if [ -n "${not_installed_packages-}" ]; then
+		msg -n "    • installing Ubuntu packages" "${not_installed_packages[@]}"
+		sudo apt -y install "${not_installed_packages[@]}" &>/dev/null & spinner
+	fi
+}
+
+
 sudo echo -n
 parse_params "$@"
 setup_colors
 
 msg "${BOLD}Installing dotfiles\n${NOFORMAT}"
+
+install_dependencies "fontconfig"
 
 install_folders=("git" "rust" "nerdfonts" "alacritty" "zellij" "nushell" "helix" "starship")
 for folder in "${install_folders[@]}"; do
