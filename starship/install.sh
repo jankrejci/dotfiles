27bacd50eb5fd6087b24e79729a29bed6427fd3f
Jan Krejčí <krejci.cze@gmail.com>
2023-08-14 21:00:40 +0200

    starship: Change install script to install latest github release

--- starship/install.sh
+++ starship/install.sh
@@ -1,30 +1,17 @@
 #!/usr/bin/env bash
 
-# Get the script location as it can be run from different place.
+# Get the script location as it can be run from different places
 script_dir=$(dirname "$(realpath "$0")")
-
 dotfiles_dir=$(dirname "$script_dir")
 . "$dotfiles_dir/common.sh"
 
 GITHUB_REPO="starship/starship"
 
-info "Installation started"
-download_from_github "$GITHUB_REPO"
-
-debug "Installing through install script"
-
-SCRIPT_NAME="starship_install.sh"
-curl -sS "https://starship.rs/install.sh" >"$TMP_DIR/$SCRIPT_NAME"
-chmod +x "$TMP_DIR/$SCRIPT_NAME"
-sudo "$TMP_DIR/$SCRIPT_NAME" -y >/dev/null 2>&1
-
-debug "Adding init sequence to zshrc"
-ZSHRC_FILE="$HOME/.zshrc"
-if ! grep "starship" <"$ZSHRC_FILE" >/dev/null 2>&1; then
-	echo 'eval "$(starship init zsh)"' >>"$ZSHRC_FILE"
-fi
+package_path=$(download_from_github "$GITHUB_REPO")
+extracted_package=$(extract_package "$package_path")
+install_binary "$extracted_package"
 
 debug "Linking configuration file"
 ln -sf "$script_dir/startship.toml" "$HOME/.config/"
 
-debug "Installation done"
+info "Installation done"
