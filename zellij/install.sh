ad44afa4788ae6b7a9865dbf9a5d795ff50729f2
Jan Krejčí <krejci.cze@gmail.com>
2023-08-15 08:46:15 +0200

    zellij: Use common linking function

--- zellij/install.sh
+++ zellij/install.sh
@@ -2,13 +2,11 @@
 
 # Get the script location as it can be run from different place.
 script_dir=$(dirname "$(realpath "$0")")
-
 dotfiles_dir=$(dirname "$script_dir")
 # shellcheck disable=SC1091
 . "$dotfiles_dir/common.sh"
 
 GITHUB_REPO="zellij-org/zellij"
-CONFIG_FOLDER="$HOME/.config/zellij"
 
 install_from_binary() {
 	debug "Downloading precompiled binary"
@@ -23,18 +21,11 @@ install_from_binary() {
 	debug "Installed into $BIN_DIR"
 }
 
-link_configuration_files() {
-	debug "Linking configuration files"
-	mkdir --parents "$CONFIG_FOLDER"
-	ln -sf "$script_dir/config.kdl" "$CONFIG_FOLDER"
-	rm -rf "$CONFIG_FOLDER/themes"
-	ln -sf "$script_dir/themes" "$CONFIG_FOLDER"
-	rm -rf "$CONFIG_FOLDER/layouts"
-	ln -sf "$script_dir/layouts" "$CONFIG_FOLDER"
-}
-
 debug "Installation started"
 apt_install "fonts-powerline"
 install_from_binary
-link_configuration_files
+link_configuration_files \
+	"config.kdl" \
+	"themes" \
+	"layouts"
 debug "Installation done"
