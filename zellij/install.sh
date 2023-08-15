d5594a0553d0765b3a0bb1a8ca47c63cf2e7c7f6
Jan Krejčí <krejci.cze@gmail.com>
2023-08-15 09:13:04 +0200

    zellij: Use common github download and install function

--- zellij/install.sh
+++ zellij/install.sh
@@ -8,23 +8,15 @@ dotfiles_dir=$(dirname "$script_dir")
 
 GITHUB_REPO="zellij-org/zellij"
 
-install_from_binary() {
-	debug "Downloading precompiled binary"
-	download_from_github "$GITHUB_REPO"
-	package_path=$(find "$TMP_DIR" -name 'zellij*')
-
-	package_name=$(basename "$package_path")
-	debug "Downloaded package $package_name"
-
-	tar -xf "$package_path" -C "$TMP_DIR"
-	install_binary "$TMP_DIR/zellij"
-	debug "Installed into $BIN_DIR"
-}
-
 apt_install "fonts-powerline"
-install_from_binary
+
+package_path=$(download_from_github "$GITHUB_REPO")
+extracted_package=$(extract_package "$package_path")
+install_binary "$extracted_package/zellij"
+
 link_configuration_files \
 	"config.kdl" \
 	"themes" \
 	"layouts"
+
 info "Installation done"
