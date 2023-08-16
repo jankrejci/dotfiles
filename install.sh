52d1d01ae8d1624191c29eb2cd96bfa5e2e61f82
Jan Krejčí <krejci.cze@gmail.com>
2023-08-16 08:59:10 +0200

    common: Add marksman package installation

--- install.sh
+++ install.sh
@@ -6,7 +6,7 @@ script_dir=$(dirname "$(realpath "$0")")
 # shellcheck disable=SC1091
 . "./common.sh"
 
-install_folders="git rust nerdfonts alacritty zellij nushell helix starship"
+install_folders="git rust nerdfonts alacritty zellij nushell helix marksman starship"
 
 for folder in $install_folders; do
 	cd "$script_dir/$folder" && ./install.sh "$@"
