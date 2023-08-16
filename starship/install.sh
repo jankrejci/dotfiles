69641c924ca61934a5f8615f5abe0ba9d11dd60d
Jan Krejčí <krejci.cze@gmail.com>
2023-08-16 08:59:10 +0200

    starship: Change install script to posix compliant

--- starship/install.sh
+++ starship/install.sh
@@ -1,12 +1,11 @@
-#!/usr/bin/env bash
+#!/usr/bin/env sh
 # shellcheck disable=SC2016
 
 # Get the script location as it can be run from different place.
 script_dir=$(dirname "$(realpath "$0")")
 
 dotfiles_dir=$(dirname "$script_dir")
-# shellcheck disable=SC1091
-source "$dotfiles_dir/common.sh"
+. "$dotfiles_dir/common.sh"
 
 msg "${BOLD}Starship installation${NOFORMAT}"
 
@@ -15,12 +14,12 @@ msg -n "    • installing through install script"
 SCRIPT_NAME="starship_install.sh"
 curl -sS "https://starship.rs/install.sh" >"$TMP_DIR/$SCRIPT_NAME"
 chmod +x "$TMP_DIR/$SCRIPT_NAME"
-sudo "$TMP_DIR/$SCRIPT_NAME" -y &>/dev/null &
+sudo "$TMP_DIR/$SCRIPT_NAME" -y >/dev/null 2>&1 &
 spinner
 
 msg "    • adding init sequence to zshrc"
 ZSHRC_FILE="$HOME/.zshrc"
-if ! grep "starship" <"$ZSHRC_FILE" &>/dev/null; then
+if ! grep "starship" <"$ZSHRC_FILE" >/dev/null 2>&1; then
 	echo 'eval "$(starship init zsh)"' >>"$ZSHRC_FILE"
 fi
 
