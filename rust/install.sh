6d684add3538403ed532e3923397341d12199d97
Jan Krejčí <krejci.cze@gmail.com>
2023-08-16 08:59:10 +0200

    Remove common methods from rust install and source it from common.sh

--- rust/install.sh
+++ rust/install.sh
@@ -1,91 +1,10 @@
 #!/usr/bin/env bash
-set -Eeuo pipefail
 
-trap cleanup SIGINT SIGTERM ERR EXIT
+# Get the script location as it can be run from different place.
+script_dir=$(dirname $(realpath "$0"))
 
-cleanup() {
-	trap - SIGINT SIGTERM ERR EXIT
-	tput cnorm
-	# rm -rf "$TMP_DIR"
-}
-
-die() {
-	local msg=$1
-	local code=${2-1}
-	msg "$msg"
-	exit "$code"
-}
-
-setup_colors() {
-	if [[ -t 2 ]] && [[ -z "${NO_COLOR-}" ]] && [[ "${TERM-}" != "dumb" ]]; then
-		NOFORMAT='\033[0m'
-		RED='\033[0;31m'
-		BOLD='\033[1m'
-	else
-		NOFORMAT='' RED='' BOLD=''
-	fi
-}
-
-spinner() {
-	local LC_CTYPE=C
-	local pid=$!
-
-	local spin='⣾⣽⣻⢿⡿⣟⣯⣷'
-	local char_width=3
-
-	local i=0
-	tput civis
-	msg -n " "
-	while kill -0 "$pid" 2>/dev/null; do
-		local i=$(((i + char_width) % ${#spin}))
-		printf "%s" "${spin:$i:$char_width}"
-		msg -n "\033[1D"
-		sleep .1
-	done
-	tput cnorm
-	msg " "
-	wait "$pid"
-	return $?
-}
-
-msg() {
-	echo >&2 -e "$@"
-}
-
-usage() {
-	cat <<EOF
-Usage: $(basename "${BASH_SOURCE[0]}")
-
-Installs Rust through rustup and cargo_binstall.
-
-Available options:
-
--f, --flag      Some flag description
--p, --param     Some param description
--h, --help      Print this help and exit
-    --debug     Print script debug info
-    --no-color  Print without colors
-EOF
-	exit
-}
-
-parse_params() {
-	while :; do
-		case "${1-}" in
-		-h | --help) usage ;;
-		--debug) set -x ;;
-		--no-color) NO_COLOR=1 ;;
-		-?*) die "Unknown option: $1" ;;
-		*) break ;;
-		esac
-		shift
-	done
-	return 0
-}
-
-sudo echo -n
-parse_params "$@"
-setup_colors
+dotfiles_dir=$(dirname "$script_dir")
+source "$dotfiles_dir/common.sh"
 
 install_rustup() {
 	rustup_init="$TMP_DIR/rustup-init.sh"
