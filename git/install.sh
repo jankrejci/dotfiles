3c48513de6417f03da653b5724dc15b7601e2ef2
Jan Krejčí <krejci.cze@gmail.com>
2023-08-16 08:59:10 +0200

    Remove common methods from git install and source it from common script

--- git/install.sh
+++ git/install.sh
@@ -1,70 +1,12 @@
 #!/usr/bin/env bash
 
-set -Eeuo pipefail
+# Get the script location as it can be run from different place.
+script_dir=$(dirname $(realpath "$0"))
 
-trap cleanup SIGINT SIGTERM ERR EXIT
+dotfiles_dir=$(dirname "$script_dir")
+source "$dotfiles_dir/common.sh"
 
-cleanup() {
-	trap - SIGINT SIGTERM ERR EXIT
-	tput cnorm
-	# Script cleanup here
-}
-
-script_dir=$(cd "$(dirname "${BASH_SOURCE[0]}")" &>/dev/null && pwd -P)
-
-die() {
-	local msg=$1
-	local code=${2-1}
-	msg "$msg"
-	exit "$code"
-}
-
-setup_colors() {
-	if [[ -t 2 ]] && [[ -z "${NO_COLOR-}" ]] && [[ "${TERM-}" != "dumb" ]]; then
-		NOFORMAT='\033[0m' BOLD='\033[1m'
-	else
-		NOFORMAT='' BOLD=''
-	fi
-}
-
-msg() {
-	echo >&2 -e "$@"
-}
-
-usage() {
-	cat <<EOF
-Usage: $(basename "${BASH_SOURCE[0]}")
-
-Batch install oo the environment.
-
-Available options:
-
--h, --help      Print this help and exit
-    --debug     Print script debug info
-    --no-color  Print without colors
-EOF
-	exit
-}
-
-parse_params() {
-    CONFIG_FOLDER="$HOME/.config/git"
-
-	while :; do
-		case "${1-}" in
-		-h | --help) usage ;;
-		--debug) set -x ;;
-		--no-color) NO_COLOR=1 ;;
-		-?*) die "Unknown option: $1" ;;
-		*) break ;;
-		esac
-		shift
-	done
-	return 0
-}
-
-sudo echo -n
-parse_params "$@"
-setup_colors
+CONFIG_FOLDER="$HOME/.config/git"
 
 msg "${BOLD}Git installation${NOFORMAT}"
 
@@ -73,4 +15,4 @@ sudo apt -y install git &>/dev/null
 
 echo "    • linking configuration files"
 mkdir --parents $CONFIG_FOLDER
-ln -sf $PWD/config $CONFIG_FOLDER
+ln -sf $script_dir/config $CONFIG_FOLDER
