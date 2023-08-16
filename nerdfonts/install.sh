534156032316d01b2335581ae14fea58fe73895e
Jan Krejčí <krejci.cze@gmail.com>
2023-08-16 08:59:10 +0200

    Remove common methods fron nerd font install and source it from common.sh

--- nerdfonts/install.sh
+++ nerdfonts/install.sh
@@ -1,78 +1,20 @@
 #!/usr/bin/env bash
 
-set -Eeuo pipefail
+# Get the script location as it can be run from different place.
+script_dir=$(dirname $(realpath "$0"))
 
-trap cleanup SIGINT SIGTERM ERR EXIT
+dotfiles_dir=$(dirname "$script_dir")
+source "$dotfiles_dir/common.sh"
 
-cleanup() {
-	trap - SIGINT SIGTERM ERR EXIT
-	tput cnorm
-	# Script cleanup here
-}
+FONT_NAME="DejaVuSansMono"
+FONT_FOLDER="$HOME/.local/share/fonts"
 
-script_dir=$(cd "$(dirname "${BASH_SOURCE[0]}")" &>/dev/null && pwd -P)
+GITHUB_REPO="ryanoasis/nerd-fonts"
+GITHUB_API="https://api.github.com/repos/$GITHUB_REPO/releases/latest"
 
-die() {
-	local msg=$1
-	local code=${2-1}
-	msg "$msg"
-	exit "$code"
-}
+VERSION=$(curl -s "$GITHUB_API" | grep -Po '"tag_name": "v\K[^"]*')
 
-setup_colors() {
-	if [[ -t 2 ]] && [[ -z "${NO_COLOR-}" ]] && [[ "${TERM-}" != "dumb" ]]; then
-		NOFORMAT='\033[0m' BOLD='\033[1m'
-	else
-		NOFORMAT='' BOLD=''
-	fi
-}
-
-msg() {
-	echo >&2 -e "$@"
-}
-
-usage() {
-	cat <<EOF
-Usage: $(basename "${BASH_SOURCE[0]}")
-
-Batch install oo the environment.
-
-Available options:
-
--h, --help      Print this help and exit
-    --debug     Print script debug info
-    --no-color  Print without colors
-EOF
-	exit
-}
-
-parse_params() {
-    FONT_NAME="DejaVuSansMono"
-    FONT_FOLDER="$HOME/.local/share/fonts"
-
-    GITHUB_REPO="ryanoasis/nerd-fonts"
-    GITHUB_API="https://api.github.com/repos/$GITHUB_REPO/releases/latest"
-
-    VERSION=$(curl -s "$GITHUB_API" | grep -Po '"tag_name": "v\K[^"]*')
-
-    GITHUB_RELEASE="https://github.com/$GITHUB_REPO/releases/download/v$VERSION/$FONT_NAME.zip"
-
-	while :; do
-		case "${1-}" in
-		-h | --help) usage ;;
-		--debug) set -x ;;
-		--no-color) NO_COLOR=1 ;;
-		-?*) die "Unknown option: $1" ;;
-		*) break ;;
-		esac
-		shift
-	done
-	return 0
-}
-
-sudo echo -n
-parse_params "$@"
-setup_colors
+GITHUB_RELEASE="https://github.com/$GITHUB_REPO/releases/download/v$VERSION/$FONT_NAME.zip"
 
 msg "${BOLD}Nerd font installation${NOFORMAT}"
 
