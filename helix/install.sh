f0cb74aa658e17d5e4c2cf86ac9d1261e1f9e6c8
Jan Krejčí <krejci.cze@gmail.com>
2023-08-16 08:59:10 +0200

    helix: Remove marksman installation from helix

--- helix/install.sh
+++ helix/install.sh
@@ -19,45 +19,6 @@ install_from_binary(){
 	rsync -a "$extracted_package/runtime" "$CONFIG_FOLDER/"
 }
 
-install_marksman() {
-	MARKSMAN_GITHUB_REPO="artempyanykh/marksman"
-
-	debug "Downloading from github repository $MARKSMAN_GITHUB_REPO"
-
-	platform=$(uname -m)
-	debug "Detected platform $platform"
-	case "$platform" in
-	  "x86_64")
-	    platform="x64"
-	    ;;
-	  "aarch64")
-	    platform="arm64"
-	    ;;
-	  *)
-	    die "Platform is not supported"
-	    ;;
-	esac
-
-	api="https://api.github.com/repos/$MARKSMAN_GITHUB_REPO/releases/latest"
-	version=$(curl -s "$api" | grep -Po "\"tag_name\": \"\K[^\"]*")
-	debug "Found latest version $version"
-
-	# Download github api json for the specified repository
-	package=$(curl -s "$api")
-	# Filter out only package download links, each link on separate line
-	package=$(echo "$package" | grep -Po "\"browser_download_url\":.*\"\K.*(?=\")" | tr " " "\n")
-	# Filter only packages relevant for the platform
-	package=$(echo "$package" | grep "$platform" | grep "linux")
-	
-	# If there is multiple packages remaining filter out gnu ve
-	curl -LJOsSf "$package" --output-dir "$TMP_DIR"
-
-	package_path=$(find "$TMP_DIR" -name 'marksman*')
-	mv "$package_path" "$TMP_DIR/marksman"
-	install_binary "$TMP_DIR" "marksman"
-	debug "Installed into $BIN_DIR"
-}
-
 install_additional_components() {
 	debug "Installing additional components"
 
@@ -70,9 +31,6 @@ install_additional_components() {
 	debug "Installing Bash LSP (bash-language-server)"
 	# TODO install npm 16 first
 	sudo npm install --silent -g bash-language-server >/dev/null 2>&1
-
-	debug "Installing Markdown LSP (marksman)"
-	install_marksman
 
 	debug "Installing Python LSP"
 	pip install python-lsp-server >/dev/null 2>&1
