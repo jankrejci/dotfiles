083dc63f6f7208753c35549c6b6fbfa72f078e18
Jan Krejčí <krejci.cze@gmail.com>
2023-08-16 08:59:09 +0200

    zellij: Install from precompiled binary
     - install ubuntu packages at first
     - change installation from source to installation from binary

--- zellij/install.sh
+++ zellij/install.sh
@@ -3,13 +3,17 @@
 CONFIG_FOLDER="$HOME/.config/zellij"
 
 echo "Zellij installation"
-echo "    • compiling from source, it may take a while"
-cargo install --quiet --locked zellij
 
 echo "    • installing dependencies for Ubuntu"
 sudo apt -y install \
 fonts-powerline \
 &> /dev/null
+
+# echo "    • compiling from source, it may take a while"
+# cargo install --quiet --locked zellij
+
+echo "    • installing from precompiled binary"
+cargo binstall -y zellij &> /dev/null
 
 echo "    • linking configuration files"
 mkdir --parents $CONFIG_FOLDER
