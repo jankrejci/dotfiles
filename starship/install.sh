ac3e2de0d0b1827b8c9c9354063eb9ef3bc9dfc3
Jan Krejčí <krejci.cze@gmail.com>
2023-08-16 08:59:09 +0200

    starship: Add installation script

--- starship/install.sh
+++ starship/install.sh
@@ -22,17 +22,9 @@ die() {
 
 setup_colors() {
 	if [[ -t 2 ]] && [[ -z "${NO_COLOR-}" ]] && [[ "${TERM-}" != "dumb" ]]; then
-		NOFORMAT='\033[0m'
-		RED='\033[0;31m'
-		GREEN='\033[0;32m'
-		ORANGE='\033[0;33m'
-		BLUE='\033[0;34m'
-		PURPLE='\033[0;35m'
-		CYAN='\033[0;36m'
-		YELLOW='\033[1;33m'
-		BOLD='\033[1m'
+		NOFORMAT='\033[0m' BOLD='\033[1m'
 	else
-		NOFORMAT='' RED='' GREEN='' ORANGE='' BLUE='' PURPLE='' CYAN='' YELLOW='' BOLD=''
+		NOFORMAT='' BOLD=''
 	fi
 }
 
@@ -44,7 +36,8 @@ spinner() {
 	local char_width=3
 
 	local i=0
-	tput civis;	msg -n " ";
+	tput civis
+	msg -n " "
 	while kill -0 "$pid" 2>/dev/null; do
 		local i=$(((i + char_width) % ${#spin}))
 		printf "%s" "${spin:$i:$char_width}"
@@ -52,7 +45,8 @@ spinner() {
 		echo -en "\033[1D"
 		sleep .1
 	done
-	tput cnorm;	msg " ";
+	tput cnorm
+	msg " "
 	wait "$pid"
 	return $?
 }
@@ -110,7 +104,6 @@ sudo echo -n
 setup_colors
 parse_params "$@"
 
-
 msg "${BOLD}Starship installation${NOFORMAT}"
 
 TMP_DIR="/tmp/dotfiles"
@@ -126,8 +119,8 @@ spinner
 
 msg "    • adding init sequence to zshrc"
 ZSHRC_FILE="$HOME/.zshrc"
-if ! grep "starship" < "$ZSHRC_FILE" &>/dev/null; then
-	echo 'eval "$(starship init zsh)"' >>"$ZSHRC_FILE" 
+if ! grep "starship" <"$ZSHRC_FILE" &>/dev/null; then
+	echo 'eval "$(starship init zsh)"' >>"$ZSHRC_FILE"
 fi
 
 msg "    • linking configuration file"
