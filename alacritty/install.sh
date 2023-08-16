c7b384256e3916f9d9e819e9fe4ceb0e43ccf059
Jan Krejčí <krejci.cze@gmail.com>
2023-08-16 08:59:09 +0200

    all: Fix install script formating with shfmt

--- alacritty/install.sh
+++ alacritty/install.sh
@@ -5,21 +5,21 @@ CONFIG_FOLDER="$HOME/.config/alacritty"
 echo "Alacritty installation"
 # Workaround to detect rpi platform
 if [[ -f /usr/bin/rpi-update ]]; then
-    echo "    • Raspberry Pi platform detected"
-    echo "    • skipping install"
-    exit;
+	echo "    • Raspberry Pi platform detected"
+	echo "    • skipping install"
+	exit
 fi
 
 echo "    • installing dependencies"
 sudo apt -y install \
-cmake \
-pkg-config \
-libfreetype6-dev \
-libfontconfig1-dev \
-libxcb-xfixes0-dev \
-libxkbcommon-dev \
-python3 \
-&> /dev/null
+	cmake \
+	pkg-config \
+	libfreetype6-dev \
+	libfontconfig1-dev \
+	libxcb-xfixes0-dev \
+	libxkbcommon-dev \
+	python3 \
+	&>/dev/null
 
 echo "    • compiling from source, it may take a while"
 cargo install --quiet alacritty
--- install.sh
+++ install.sh
@@ -4,33 +4,32 @@
 sudo echo
 
 echo "Installing dotfiles"
-BASE_FOLDER=`pwd`
+BASE_FOLDER=$(pwd)
 CARGO_BIN="$HOME/.cargo/bin"
 
 echo "    • installing Ubuntu packages"
-sudo apt -y install npm &> /dev/null
+sudo apt -y install npm &>/dev/null
 
 echo "    • installing Rustup"
 RUSTUP_INIT="/tmp/rustup-init.sh"
-curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs > $RUSTUP_INIT
+curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs >$RUSTUP_INIT
 chmod +x $RUSTUP_INIT
-$RUSTUP_INIT -y &> /dev/null
+$RUSTUP_INIT -y &>/dev/null
 rm $RUSTUP_INIT
 PATH=$PATH:$HOME/.cargo/bin/
 
-
-DETECTED_PLATFORM=`uname -m`
+DETECTED_PLATFORM=$(uname -m)
 case ${DETECTED_PLATFORM} in
-  "x86_64")
-     CARGO_BINSTALL="https://github.com/cargo-bins/cargo-binstall/releases/latest/download/cargo-binstall-x86_64-unknown-linux-musl.tgz"
-     ;;
-  "aarch64")
-     CARGO_BINSTALL="https://github.com/cargo-bins/cargo-binstall/releases/latest/download/cargo-binstall-aarch64-unknown-linux-musl.tgz"
-     ;;
-  *)
-     echo "    • unknown platform"
-     exit
-     ;;
+"x86_64")
+	CARGO_BINSTALL="https://github.com/cargo-bins/cargo-binstall/releases/latest/download/cargo-binstall-x86_64-unknown-linux-musl.tgz"
+	;;
+"aarch64")
+	CARGO_BINSTALL="https://github.com/cargo-bins/cargo-binstall/releases/latest/download/cargo-binstall-aarch64-unknown-linux-musl.tgz"
+	;;
+*)
+	echo "    • unknown platform"
+	exit
+	;;
 esac
 echo "    • detected platform $DETECTED_PLATFORM"
 
@@ -43,7 +42,6 @@ tar zxf *
 mv cargo-binstall $CARGO_BIN
 cd $BASE_FOLDER
 rm -rf $TMP_DIR
-
 
 cd $BASE_FOLDER/git && ./install.sh
 cd $BASE_FOLDER/nerdfonts && ./install.sh
