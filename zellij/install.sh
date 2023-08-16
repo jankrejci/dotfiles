3ab1cbd1d861e34acde3ae1c8c6607ef1cb678ce
Jan Krejčí <krejci.cze@gmail.com>
2023-08-16 08:59:09 +0200

    zellij: Fix intendation

--- zellij/install.sh
+++ zellij/install.sh
@@ -8,38 +8,38 @@ print_help(){
 }
 
 process_args() {
-  eval set -- "$VALID_ARGS"
-  while [ : ]; do
-    case "$1" in
-      -s | --source)
-          BUILD_FROM_SOURCE=true
-          shift
-          ;;
-      -h | --help)
-          print_help
-          shift
-          ;;
-      --)
-          shift; 
-          break 
-          ;;
-    esac
-  done
+    eval set -- "$VALID_ARGS"
+    while [ : ]; do
+        case "$1" in
+            -s | --source)
+                BUILD_FROM_SOURCE=true
+                shift
+                ;;
+            -h | --help)
+                print_help
+                shift
+                ;;
+            --)
+                shift; 
+                break 
+                ;;
+        esac
+    done
 }
 
 install_dependencies() {
-  echo "    • installing Ubuntu dependencies"
-  sudo apt -y install fonts-powerline &> /dev/null
+    echo "    • installing Ubuntu dependencies"
+    sudo apt -y install fonts-powerline &> /dev/null
 }
 
 install_from_binary() {
-  echo "    • installing from precompiled binary"
-  cargo binstall -y zellij &> /dev/null
+    echo "    • installing from precompiled binary"
+    cargo binstall -y zellij &> /dev/null
 }
 
 install_from_source() {
-  echo "    • compiling from source, it may take a while"
-  cargo install --quiet --locked zellij
+    echo "    • compiling from source, it may take a while"
+    cargo install --quiet --locked zellij
 }
 
 VALID_ARGS=$(getopt -o sh --long source,help -- "$@")
@@ -54,9 +54,9 @@ echo "Zellij installation"
 install_dependencies
 
 if [ $BUILD_FROM_SOURCE == true ]; then
-  install_from_source
+    install_from_source
 else
-  install_from_binary
+    install_from_binary
 fi
 
 echo "    • linking configuration files"
