c93515adce5fd1ed6457e5372416bd8e602e4781
Jan Krejčí <krejci.cze@gmail.com>
2024-02-19 16:37:00 +0100

    alacritty: Modify desktop entry to use nixGL

--- nix/home-manager/apps/alacritty.nix
+++ nix/home-manager/apps/alacritty.nix
@@ -5,4 +5,15 @@
             font.normal.family = "DejaVuSansMono NF";
     		};
     };
+
+   xdg.desktopEntries."Alacritty" = {
+        name = "Alacritty";
+        comment = "Terminal emulator";
+        icon = "Alacritty";
+        exec = "nixGL alacritty";
+        categories = [ "TerminalEmulator" ];
+        terminal = false;
+        mimeType = [ "text/plain" ];
+    };
+
 }
