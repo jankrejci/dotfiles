3081edcbafba0ffdc46cef772816a6a153eecbf3
Jan Krejčí <krejci.cze@gmail.com>
2024-02-19 16:36:08 +0100

    nix: Add nixGL overlay

--- nix/home-manager/flake.nix
+++ nix/home-manager/flake.nix
@@ -1,29 +1,36 @@
 {
-  description = "Home Manager configuration of jkr";
+    description = "Home Manager configuration of jkr";
 
-  inputs = {
-    # Specify the source of Home Manager and Nixpkgs.
-    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
-    home-manager = {
-      url = "github:nix-community/home-manager";
-      inputs.nixpkgs.follows = "nixpkgs";
+    inputs = {
+        # Specify the source of Home Manager and Nixpkgs.
+        nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+        home-manager = {
+            url = "github:nix-community/home-manager";
+            inputs.nixpkgs.follows = "nixpkgs";
+        };
+        # nixgl is needed for alacritty outside of nixOS
+        # refer to https://github.com/NixOS/nixpkgs/issues/122671
+        # https://github.com/guibou/nixGL/#use-an-overlay
+        nixgl.url = "github:guibou/nixGL";
     };
-  };
 
-  outputs = { nixpkgs, home-manager, ... }:
-    let
-      system = "x86_64-linux";
-      pkgs = nixpkgs.legacyPackages.${system};
-    in {
-      homeConfigurations."jkr" = home-manager.lib.homeManagerConfiguration {
-        inherit pkgs;
+    outputs = { nixpkgs, home-manager, nixgl, ... }:
+        let
+            system = "x86_64-linux";
+            pkgs = import nixpkgs {
+                  system = "x86_64-linux";
+                  overlays = [ nixgl.overlay ];
+            };
+        in {
+            homeConfigurations."jkr" = home-manager.lib.homeManagerConfiguration {
+            inherit pkgs;
 
-        # Specify your home configuration modules here, for example,
-        # the path to your home.nix.
-        modules = [ ./home.nix ];
+            # Specify your home configuration modules here, for example,
+            # the path to your home.nix.
+            modules = [ ./home.nix ];
 
-        # Optionally use extraSpecialArgs
-        # to pass through arguments to home.nix
-      };
+            # Optionally use extraSpecialArgs
+            # to pass through arguments to home.nix
+        };
     };
 }
