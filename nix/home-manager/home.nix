3634fadd6e18047224a4320e528624cc6c423605
Jan Krejčí <krejci.cze@gmail.com>
2024-02-19 17:14:34 +0100

    nerdfonts: Install fonts through nix

--- nix/home-manager/home.nix
+++ nix/home-manager/home.nix
@@ -31,6 +31,8 @@
     nixgl.nixGLIntel
   ];
 
+  fonts.fontconfig.enable = true; 
+
   # Home Manager is pretty good at managing dotfiles. The primary way to manage
   # plain files is through 'home.file'.
   home.file = {
