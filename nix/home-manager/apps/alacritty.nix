58fe6f2453ae6db3911df03a330fb47d97f7be31
Jan Krejčí <krejci.cze@gmail.com>
2024-02-20 10:47:21 +0100

    nix: Change indentation of all nix files, use nixfmt

--- nix/home-manager/apps/alacritty.nix
+++ nix/home-manager/apps/alacritty.nix
@@ -1,20 +1,20 @@
 {
-    programs.alacritty= {
-        enable = true;
-    		settings = {
-            font.normal.family = "DejaVuSansMono NF";
-            window.decorations = "None";
-    		};
+  programs.alacritty = {
+    enable = true;
+    settings = {
+      font.normal.family = "DejaVuSansMono NF";
+      window.decorations = "None";
     };
+  };
 
-   xdg.desktopEntries."Alacritty" = {
-        name = "Alacritty";
-        comment = "Terminal emulator";
-        icon = "Alacritty";
-        exec = "nixGL alacritty";
-        categories = [ "TerminalEmulator" ];
-        terminal = false;
-        mimeType = [ "text/plain" ];
-    };
+  xdg.desktopEntries."Alacritty" = {
+    name = "Alacritty";
+    comment = "Terminal emulator";
+    icon = "Alacritty";
+    exec = "nixGL alacritty";
+    categories = [ "TerminalEmulator" ];
+    terminal = false;
+    mimeType = [ "text/plain" ];
+  };
 
 }
--- nix/home-manager/apps/starship.nix
+++ nix/home-manager/apps/starship.nix
@@ -1,40 +1,40 @@
 {
-    programs.starship = {
-        enable = true;
-        enableNushellIntegration = true;
-        enableBashIntegration=true;
-        settings = {
-            c = {
-                symbol = " ";
-            };
-            directory = {
-                truncation_length = 1;
-                read_only = " ";
-            };
-            git_branch = {
-                symbol = " ";
-            };
-            os.symbols = {
-                Arch = " ";
-                Debian = " ";
-                Linux = " ";
-                Macos = " ";
-                Raspbian = " ";
-                Ubuntu = " ";
-                Unknown = " ";
-                Windows = " ";
-            };
-            package ={
-                symbol = " ";
-                disabled = true;
-            };
-            python = {
-                symbol = " ";
-            };
-            rust = {
-                symbol = " ";
-                format = "[ $version](red bold)";
-            };
-    		};
+  programs.starship = {
+    enable = true;
+    enableNushellIntegration = true;
+    enableBashIntegration = true;
+    settings = {
+      c = {
+        symbol = " ";
+      };
+      directory = {
+        truncation_length = 1;
+        read_only = " ";
+      };
+      git_branch = {
+        symbol = " ";
+      };
+      os.symbols = {
+        Arch = " ";
+        Debian = " ";
+        Linux = " ";
+        Macos = " ";
+        Raspbian = " ";
+        Ubuntu = " ";
+        Unknown = " ";
+        Windows = " ";
+      };
+      package = {
+        symbol = " ";
+        disabled = true;
+      };
+      python = {
+        symbol = " ";
+      };
+      rust = {
+        symbol = " ";
+        format = "[ $version](red bold)";
+      };
     };
+  };
 }
