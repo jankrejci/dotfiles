65a30bf726b9234e0ffb5569643dd3ba77401f76
Jan Krejčí <krejci.cze@gmail.com>
2024-05-01 15:03:31 +0200

    git: Change git aliases
    - remove unused aliases
    - modify ll alias to show references
    - add sh alias for git show

--- apps/git.nix
+++ apps/git.nix
@@ -4,23 +4,21 @@
     delta.enable = true;
     extraConfig.core.editor = "hx";
     aliases = {
-      ll = "log --pretty=format:'%Cred%h% %Cgreen(%ad) %C(bold blue)<%<(18)%an>%Creset %s' --abbrev-commit --date=format:'%Y-%m-%d'";
+      ll = "log --pretty=format:'%Cred%h %Cgreen(%ad) %C(bold blue)<%an>%Creset %s %C(yellow)%d%Creset' --abbrev-commit --date=format:'%Y-%m-%d'";
       ri = "rebase --interactive";
       ria = "rebase --interactive --autosquash";
-      ra = "rebase --abort";
       riam = "rebase --interactive --autosquash origin/master";
-      ram = "rebase --abort origin/master";
+      ra = "rebase --abort";
       rc = "rebase --continue";
       ap = "add -p";
-      cf = "commit --fixup";
-      cfh = "commit --fixup=HEAD";
       c = "commit";
+      cfh = "commit --fixup=HEAD";
       fm = "!git fetch; git checkout origin/master";
       f = "fetch";
       st = "status";
+      sh = "show";
       co = "checkout";
-      cob = "ceckout -b";
-
+      cob = "checkout -b";
     };
   };
 }
