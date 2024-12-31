431b44017b405ceba32f44ce9f217d5090f7e6e9
jankrejci <krejcijan@protonmail.com>
2024-12-31 17:26:26 +0100

    nix: Renamde diplaylink.nix module to vieo.nix

--- nixos/video.nix
+++ nixos/video.nix
@@ -1,8 +1,8 @@
 { pkgs, ... }:
 {
   environment.systemPackages = with pkgs; [
-    linuxKernel.packages.linux_6_6.evdi
     displaylink
   ];
+
   services.xserver.videoDrivers = [ "displayLink" "modesetting" ];
 }
