a7221401355eb068023a665cb470aca261756982
jankrejci <krejcijan@protonmail.com>
2025-01-02 19:00:55 +0100

    gnome: Enable overamplification

--- modules/users/paja/home.nix
+++ modules/users/paja/home.nix
@@ -8,6 +8,15 @@
   home.packages = with pkgs; [
   ];
 
+  dconf.settings = {
+    "org/gnome/desktop/interface" = {
+      color-scheme = "prefer-dark";
+    };
+    "org/gnome/desktop/sound" = {
+      allow-volume-above-100-percent = true;
+    };
+  };
+
   # This value determines the Home Manager release that your
   # configuration is compatible with. It's recommended to keep this up to date.
   home.stateVersion = "24.05";
