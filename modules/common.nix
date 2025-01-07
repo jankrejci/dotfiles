df0d94335a2784dcd01ddcb803f1a635d3ccf179
Jan Krejčí <jan.krejci@braiins.cz>
2025-01-07 10:21:52 +0100

    nix: Move package definitio to common and desktop modules

--- modules/common.nix
+++ modules/common.nix
@@ -1,5 +1,17 @@
-{ ... }:
+{ system, nixpkgs, nixpkgs-unstable, ... }:
+let
+  unstable = import nixpkgs-unstable {
+    inherit system;
+    config.allowUnfree = true;
+  };
 
+  pkgs = import nixpkgs {
+    inherit system;
+    config.allowUnfree = true;
+    config.nvidia.acceptLicense = true;
+    overlays = [ unstable ];
+  };
+in
 {
   nix.settings.experimental-features = [ "nix-command" "flakes" ];
 
@@ -18,4 +30,41 @@
     LC_TELEPHONE = "cs_CZ.UTF-8";
     LC_TIME = "cs_CZ.UTF-8";
   };
+
+  # Common system packages
+  environment.systemPackages = with pkgs; [
+    unstable.zellij
+    unstable.helix
+    unstable.nushell
+    unstable.broot
+    starship
+    powerline-fonts # patched fonts used for zellij
+    (pkgs.nerdfonts.override { fonts = [ "DejaVuSansMono" ]; }) # iconic fonts
+    curl
+    bash
+    home-manager
+    unzip
+    wget
+    coreutils
+    rclone
+    rsync
+    jq
+    vim
+    neofetch
+    git
+    usbutils
+    pciutils
+    lshw
+    brightnessctl
+    bat # cat clone with wings
+    eza # ls replacement
+    fd # find relplacement
+    fzf # cli fuzzy finder
+    zoxide # smarter cd command
+    ripgrep # search tool 
+    tealdeer # tldr help tool
+    nmap
+    htop
+    wireguard-tools
+  ];
 }
