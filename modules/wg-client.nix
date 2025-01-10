77dc5aff4f345e8326996120e01573e75797a9c2
jankrejci <krejcijan@protonmail.com>
2025-01-10 17:24:53 +0100

    server: Generate new wg key

--- modules/wg-client.nix
+++ modules/wg-client.nix
@@ -4,7 +4,7 @@
     peers = [
       {
         # Public key of the server
-        publicKey = "QMz5SM4JuQrW+ms9N9dtihS9TYbhWzlXQHjr/X4oRGg=";
+        publicKey = "iWfrqdXV4bDQOCfhlZ2KRS7eq2B/QI440HylPrzJUww=";
         allowedIPs = [ "192.168.99.0/24" "1.1.1.1" "8.8.8.8" ];
         endpoint = "37.205.13.227:51820";
         # Send keepalives every 25 seconds. Important to keep NAT tables alive.
