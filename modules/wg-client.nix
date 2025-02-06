cc1591cb1a47b66da0a84dd262757770c43e8b3c
jankrejci <krejcijan@protonmail.com>
2025-02-06 21:17:26 +0100

    wireguard: Cleanup the host configuration

--- modules/wg-client.nix
+++ modules/wg-client.nix
@@ -2,18 +2,15 @@
 {
   networking.wg-quick.interfaces.wg0 =
     {
-      ips = [ "${hostConfig.ipAddress}/24" ];
+      address = [ "${hostConfig.ipAddress}/24" ];
       privateKeyFile = config.sops.secrets."hosts/${hostConfig.hostName}/wg_private_key".path;
       dns = [ "192.168.99.1" "home" ];
 
       peers = [
         {
-          # Public key of the server
-          name = hostInfo.vpsfree.hostName;
           publicKey = hostInfo.vpsfree.wgPublicKey;
           allowedIPs = [ "192.168.99.0/24" ];
           endpoint = "37.205.13.227:51820";
-          # Send keepalives every 25 seconds. Important to keep NAT tables alive.
           persistentKeepalive = 25;
         }
       ];
