a0cfb33bf56ce0415f1f48f7aa3dff0fe684534c
jankrejci <krejcijan@protonmail.com>
2025-02-09 15:57:31 +0100

    nix: Move host definitions to separate file

--- flake.nix
+++ flake.nix
@@ -45,60 +45,7 @@
         overlays = [ unstable-aarch64-linux ];
       };
 
-      hostConfigs = {
-        vpsfree = {
-          ipAddress = "192.168.99.1";
-          wgPublicKey = "iWfrqdXV4bDQOCfhlZ2KRS7eq2B/QI440HylPrzJUww=";
-          system = "x86_64-linux";
-          extraModules = [
-            ./modules/users/admin/user.nix
-            ./modules/wg-server.nix
-            ./modules/grafana.nix
-            ./modules/vpsadminos.nix
-          ];
-        };
-        rpi4 = {
-          ipAddress = "192.168.99.2";
-          wgPublicKey = "sUUZ9eIfyjqdEDij7vGnOe3sFbbF/eHQqS0RMyWZU0c=";
-          system = "aarch64-linux";
-          extraModules = [
-            "${nixpkgs}/nixos/modules/installer/sd-card/sd-image-aarch64.nix"
-            ./modules/users/admin/user.nix
-            ./modules/wg-client.nix
-          ];
-        };
-        optiplex = {
-          ipAddress = "192.168.99.3";
-          wgPublicKey = "6QNJxFoSDKwqQjF6VgUEWP5yXXe4J3DORGo7ksQscFA=";
-          system = "x86_64-linux";
-          extraModules = [
-            ./modules/users/jkr/user.nix
-            ./modules/users/paja/user.nix
-            ./modules/wg-client.nix
-            ./modules/desktop.nix
-          ];
-        };
-        thinkpad = {
-          ipAddress = "192.168.99.4";
-          wgPublicKey = "IzW6yPZJdrBC6PtfSaw7k4hjH+b/GjwvwiDLkLevLDI=";
-          system = "x86_64-linux";
-          extraModules = [
-            ./modules/users/jkr/user.nix
-            ./modules/users/paja/user.nix
-            ./modules/wg-client.nix
-            ./modules/desktop.nix
-            ./modules/disable-nvidia.nix
-          ];
-        };
-        latitude = {
-          ipAddress = "192.168.99.5";
-          wgPublicKey = "ggj+uqF/vij5V+fA5r9GIv5YuT9hX7OBp+lAGYh5SyQ=";
-        };
-        android = {
-          ipAddress = "192.168.99.6";
-          wgPublicKey = "HP+nPkrKwAxvmXjrI9yjsaGRMVXqt7zdcBGbD2ji83g=";
-        };
-      };
+      hostConfigs = import ./hosts.nix;
 
       hostInfo = builtins.mapAttrs
         (hostName: config: {
