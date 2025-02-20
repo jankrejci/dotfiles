d6611d6da1476c4e62e5d15902d8d0fe08c9a91e
jankrejci <krejcijan@protonmail.com>
2025-02-20 19:39:39 +0100

    ssh: Read authorized keys from the github repository

--- hosts/rpi4/configuration.nix
+++ hosts/rpi4/configuration.nix
@@ -1,13 +1,6 @@
 { ... }:
 {
+  # TODO create raspberry.nix module and factor out common things
   security.sudo.wheelNeedsPassword = false;
-
-  # TODO find out how to manage admin keys
-  users.users.admin = {
-    openssh.authorizedKeys.keys = [
-      "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIKcPS15FwxQjt4xZJk0+VzKqLTh/rikF0ZI4GFyOTLoD jkr@optiplex-rpi4"
-      "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIIwuE1yPiKiX7SL1mSDZB6os9COPdOqrWfh9rUNBpfOq jkr@thinkpad-rpi4"
-      "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAICqz14JAcEgnxuy4xIkUiots+K1bo1uQGd/Tn7mRWyu+ jkr@latitude-rpi4"
-    ];
-  };
 }
+
