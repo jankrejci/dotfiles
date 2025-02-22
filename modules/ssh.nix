6fd3851352d1b67a474081bb9cc952af075c593e
jankrejci <krejcijan@protonmail.com>
2025-02-22 08:23:03 +0100

    ssh: Use caching for keys fetching

--- modules/ssh.nix
+++ modules/ssh.nix
@@ -2,15 +2,50 @@
 let
   gitRepo = "https://raw.githubusercontent.com/jankrejci/dotfiles";
   gitBranch = "jkr/install";
+  keysFolder = "/var/cache/ssh";
   keysFile = "ssh-authorized-keys.pub";
+  keysPath = "${keysFolder}/${keysFile}";
+  keysUser = "nobody";
+  keysGroup = "nogroup";
 
+  # Load keys from the github repository
+  # There is very limited env, use full paths
   fetchAuthorizedKeys = ''
     #!${pkgs.stdenv.shell}
 
-    "${pkgs.curl}/bin/curl" "${gitRepo}/refs/heads/${gitBranch}/hosts/${hostConfig.hostName}/${keysFile}"
+    if [ ! -f "${keysPath}" ]; then
+      "${pkgs.curl}/bin/curl" "${gitRepo}/refs/heads/${gitBranch}/hosts/${hostConfig.hostName}/${keysFile}" > "${keysPath}"
+    fi
+
+    "${pkgs.util-linux}/bin/logger" "Moc necum"
+
+    "${pkgs.coreutils}/bin/cat" "${keysPath}"
   '';
 in
 {
+  # Use the folder for the key cache, keys are valid at least 1 minute
+  systemd.tmpfiles.settings = {
+    "10-ssh-authorized-keys-cache" = {
+      "${keysFolder}" = {
+        d = {
+          mode = "0700";
+          user = keysUser;
+          group = keysGroup;
+          age = "1m";
+        };
+      };
+    };
+  };
+
+  # Invoke the cleanup every 5 minutes to get rid of outdated ssh keys
+  systemd.timers."systemd-tmpfiles-clean".timerConfig = {
+    # Time to wait after boot before first run
+    OnBootSec = "5min";
+    # Time between subsequent runs
+    OnUnitActiveSec = "5min";
+  };
+
+  # The sshds AuthorizedKeysCommand must be owned by root and not writable by others
   environment.  etc."fetch-authorized-keys" = {
     source = pkgs.writeScript "fetch-authorized-keys" fetchAuthorizedKeys;
     mode = "0555";
@@ -29,7 +64,7 @@ in
       PasswordAuthentication = false;
       PermitRootLogin = lib.mkForce "no";
       AuthorizedKeysCommand = "/etc/fetch-authorized-keys";
-      AuthorizedKeysCommandUser = "nobody";
+      AuthorizedKeysCommandUser = keysUser;
     };
   };
 
@@ -38,3 +73,4 @@ in
     RestartSec = 5;
   };
 }
+
