f21e6b2823b81b4bb66979aea030f04e5e7d93e3
jankrejci <krejcijan@protonmail.com>
2025-02-28 14:41:49 +0100

    thinkcenter: Fetch ssh keys from github

--- hosts/thinkcenter/configuration.nix
+++ hosts/thinkcenter/configuration.nix
@@ -4,12 +4,6 @@
     ./hardware-configuration.nix
   ];
 
-  users.users.admin = {
-    openssh.authorizedKeys.keys = [
-      "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIN85SndW/OerKK8u2wTxmHcTn4hEtUJmctj9wnseBYtS jkr@optiplex-vpsfree"
-    ];
-  };
-
   # Add password for the admin user to be able to log in from local console
   users.users.admin.hashedPassword = "$y$j9T$8qLqeoP/jNv9rFtFfyljl1$S/GqBaFaaCIluY88qW9app4APK49d9wFI.5CmfFnwH/";
 
