4805646c6a15476e77e268fa8cd663d50c57cfd2
jankrejci <krejcijan@protonmail.com>
2025-03-08 17:11:12 +0100

    vpsfree: Fetch ssh keys from github

--- hosts/vpsfree/configuration.nix
+++ hosts/vpsfree/configuration.nix
@@ -1,14 +1,5 @@
 { ... }:
 {
-  # TODO use the ssh-authorized-keys.pub file
-  users.users.admin = {
-    openssh.authorizedKeys.keys = [
-      "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIN85SndW/OerKK8u2wTxmHcTn4hEtUJmctj9wnseBYtS jkr@optiplex-vpsfree"
-      "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAICWjLMaGa0MgRX0K1i2+me5iWe3Kpw/9RrpWMZLDtOWr jkr@thinkpad-vpsfree"
-      "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIB/XIewPQt4B37PnUVItz8+LZ/SGWjf8JogSEZEGC5Cm jkr@latitude-vpsfree"
-    ];
-  };
-
   nix.settings.trusted-public-keys = [
     "jkr@optiplex:KwH9qv7qCHd+npxC0FUFCApM1rP6GSbB8Ze983CCT8o="
   ];
