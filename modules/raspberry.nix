9c53405aace38442e38e2e1d195ea1834615deb6
Jan Krejčí <jan.krejci@braiins.cz>
2025-03-19 13:56:18 +0100

    raspberry: Remove sops module dependency

--- modules/raspberry.nix
+++ modules/raspberry.nix
@@ -1,9 +1,5 @@
 { config, lib, pkgs, ... }:
 let
-  # Define paths to the decrypted secrets
-  ssidPath = config.sops.secrets."wifi_ssid".path;
-  pskPath = config.sops.secrets."wifi_psk".path;
-
   # Path where the wpa_supplicant configuration will be generated
   wpaConfigFolder = "/var/lib/wpa_supplicant";
   wpaConfigPath = "${wpaConfigFolder}/wpa_supplicant.conf";
@@ -47,11 +43,6 @@ in
 
   swapDevices = [ ];
 
-  sdImage = {
-    compressImage = false;
-    imageName = "${config.hosts.self.hostName}-sdcard.img";
-  };
-
   # TODO find out more unused services to disable
   # Disable unneeded features
   services = {
@@ -65,20 +56,6 @@ in
   # I dont need the bluetooth so far
   hardware.bluetooth.enable = false;
 
-  # TODO find out how to share multiple wifi config in a better way
-  sops = {
-    secrets = {
-      "wifi_ssid" = { mode = "0400"; owner = "systemd-network"; };
-      "wifi_psk" = { mode = "0400"; owner = "systemd-network"; };
-    };
-  };
-
-  # Ensure wpa_supplicant is installed
-  environment = {
-    systemPackages = with pkgs; [ wpa_supplicant ];
-    etc."wpa_supplicant.conf".source = wpaConfigPath;
-  };
-
   # Enable wpa_supplicant service
   networking = {
     wireless = {
@@ -87,32 +64,7 @@ in
     };
     supplicant.wlan0 = {
       driver = "nl80211";
-      configFile.path = "/etc/wpa_supplicant.conf";
-    };
-  };
-
-  systemd.services = {
-    # Ensure wpa_supplicant starts after the configuration is generated
-    wpa_supplicant.after = [ "wpa-generate-config.service" ];
-    # Systemd service to generate the wpa_supplicant configuration at runtime
-    "wpa-generate-config" = {
-      description = "Generate wpa_supplicant configuration from SOPS secrets";
-      after = [ "network-pre.target" ];
-      before = [ "wpa_supplicant.service" ];
-      wantedBy = [ "multi-user.target" ];
-      script = ''
-        SSID=$(cat ${ssidPath})
-        PSK=$(cat ${pskPath})
-        mkdir -p ${wpaConfigFolder}
-        cat > ${wpaConfigPath} <<EOF
-        network={
-          ssid="$SSID"
-          psk="$PSK"
-        }
-        EOF
-        chmod 600 ${wpaConfigPath}
-        chown root:root ${wpaConfigPath}
-      '';
+      configFile.path = wpaConfigPath;
     };
   };
 }
