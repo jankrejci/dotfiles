8adce919eb6a343f3c00db64b4fc6026993d2766
jankrejci <krejcijan@protonmail.com>
2025-03-20 21:53:50 +0100

    disk: Use descriptor values for the disk config

--- hosts/iso/configuration.nix
+++ hosts/iso/configuration.nix
@@ -4,7 +4,12 @@
     loader.efi.canTouchEfiVariables = true;
   };
 
-  isoImage.compressImage = false;
+  fileSystems = {
+    "/" = {
+      device = "/dev/disk/by-partlabel/nixos-minimal-24.11-x86_64";
+      fsType = "ext4";
+    };
+  };
 
   # Allow nixos-anywhere to login as root for installation
   services.openssh.settings.PermitRootLogin = lib.mkForce "yes";
