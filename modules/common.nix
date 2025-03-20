4c043a184bd3cae9146d8a557d9c86ed374d6a93
jankrejci <krejcijan@protonmail.com>
2025-03-20 21:54:24 +0100

    nix: Move the most of configuration.nix into shared modules

--- modules/common.nix
+++ modules/common.nix
@@ -5,6 +5,7 @@
   nixpkgs.hostPlatform = lib.mkDefault "x86_64-linux";
 
   boot = {
+    kernelModules = lib.mkDefault [ "kvm-intel" ];
     extraModulePackages = [ ];
     initrd = {
       availableKernelModules = [
