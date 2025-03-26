1c9a9252495b1f63ce8a3a5ed26206799440b565
jankrejci <krejcijan@protonmail.com>
2025-03-26 20:59:26 +0100

    nix: Enable microcode updates

--- hosts/thinkcenter/configuration.nix
+++ hosts/thinkcenter/configuration.nix
@@ -1,4 +1,4 @@
-{ config, ... }:
+{ ... }:
 {
   # Access for all hosts without sudo for convenience
   security.sudo.wheelNeedsPassword = false;
@@ -6,5 +6,5 @@
   # Add password for the admin user to be able to log in from local console
   users.users.admin.hashedPassword = "$y$j9T$8qLqeoP/jNv9rFtFfyljl1$S/GqBaFaaCIluY88qW9app4APK49d9wFI.5CmfFnwH/";
 
-  hardware.cpu.intel.updateMicrocode = config.hardware.enableRedistributableFirmware;
+  hardware.cpu.intel.updateMicrocode = true;
 }
