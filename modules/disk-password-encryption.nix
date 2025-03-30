7cf35f7532866c9dcb3d187084221ec40ccdb73a
jankrejci <krejcijan@protonmail.com>
2025-03-30 12:18:32 +0200

    encryption: Add disk password encryption support

---
+++ modules/disk-password-encryption.nix
@@ -0,0 +1,116 @@
+{ config, pkgs, ... }:
+let
+  luksDevice = "/dev/disk/by-partlabel/disk-main-luks";
+  diskPasswordFile = "/var/lib/disk-password";
+in
+{
+  # Boot parition and encrypted root partition
+  disko.devices = {
+    disk = {
+      main = {
+        type = "disk";
+        device = config.hosts.self.device;
+        content = {
+          type = "gpt";
+          partitions = {
+            ESP = {
+              size = "500M";
+              type = "EF00";
+              content = {
+                type = "filesystem";
+                format = "vfat";
+                mountpoint = "/boot";
+                mountOptions = [ "umask=0077" ];
+              };
+            };
+            luks = {
+              size = "100%";
+              content = {
+                type = "luks";
+                name = "crypted";
+                extraOpenArgs = [ ];
+                passwordFile = diskPasswordFile;
+                content = {
+                  type = "lvm_pv";
+                  vg = "pool";
+                };
+              };
+            };
+          };
+        };
+      };
+    };
+    lvm_vg = {
+      pool = {
+        type = "lvm_vg";
+        lvs = {
+          root = {
+            size = "100%";
+            content = {
+              type = "filesystem";
+              format = "ext4";
+              mountpoint = "/";
+              mountOptions = [
+                "defaults"
+              ];
+            };
+          };
+          swap = {
+            size = config.hosts.self.swapSize;
+            content = {
+              type = "swap";
+              discardPolicy = "both";
+            };
+          };
+        };
+      };
+    };
+  };
+
+  boot = {
+    loader = {
+      systemd-boot = {
+        # Systemd boot instead of GRUB is needed for secure boot,
+        # to use secure boot with GRUB, you need to use Lanzaboote project
+        enable = true;
+        # Avoid too many bootloader generations
+        # that can consume all the /boot partition space
+        configurationLimit = 3;
+        # Disable boot menu editing
+        editor = false;
+      };
+      efi.canTouchEfiVariables = true;
+    };
+
+    initrd = {
+      systemd.enable = true;
+      luks.devices."cryptroot" = {
+        device = luksDevice;
+        preLVM = true;
+        allowDiscards = true;
+      };
+    };
+  };
+
+  # Final TPM key enrollment
+  systemd.services."erase-temporary-password" = {
+    description = "Get rid of the temporary disk password";
+    wantedBy = [ "multi-user.target" ];
+
+    serviceConfig = {
+      Type = "oneshot";
+      RemainAfterExit = true;
+      ConditionPathExists = "!/run/initramfs";
+    };
+
+    script = ''
+      if [ ! -f "${diskPasswordFile}" ]; then
+        echo "Password file "${diskPasswordFile}" already cleaned up"
+        exit 0
+      fi
+
+      # Clean up temporary file securely
+      ${pkgs.coreutils}/bin/shred -u "${diskPasswordFile}"
+    '';
+  };
+}
