7f450eb97407debc6e706f9e747e4e1a658ea633
Jan Krejci <krejcijan@protonmail.com>
2025-11-13 10:11:03 +0100

    notebook: Introduce new module for notebook configuration

--- hosts/thinkpad/configuration.nix
+++ hosts/thinkpad/configuration.nix
@@ -6,37 +6,19 @@
     speed = 120;
   };
 
-  services.thermald.enable = true;
-  powerManagement.powertop.enable = true;
-
-  # For better hardware compatibility
-  hardware.enableAllFirmware = true;
   hardware.cpu.intel.updateMicrocode = true;
-
-  # Enable non-free firmware
-  hardware.enableRedistributableFirmware = true;
 
   # ThinkPad E470 keyboard backlight
   hardware.acpilight.enable = true;
 
   # Graphics - E470 typically has Intel HD Graphics 620
   hardware.graphics = {
-    enable = true;
     extraPackages = with pkgs; [
       intel-media-driver
       vaapiIntel
       libvdpau-va-gl
     ];
   };
-
-  # Add support for the fingerprint reader
-  services.fprintd.enable = true;
-
-  # Wireless drivers support - the E470 uses Intel wireless cards
-  hardware.firmware = with pkgs; [
-    linux-firmware
-    firmwareLinuxNonfree
-  ];
 
   # Sleep/hibernation optimization
   boot.kernelParams = [
