7776e463c432cb3bcb41625f3ce979785883d967
Jan Krejci <krejcijan@protonmail.com>
2025-11-14 08:56:12 +0100

    nix: Use the Ng switch

--- modules/common.nix
+++ modules/common.nix
@@ -8,6 +8,11 @@
 
   nixpkgs.hostPlatform = lib.mkDefault "x86_64-linux";
 
+  system.switch = {
+    enable = false;
+    enableNg = true;
+  };
+
   # Consider users with the root access trustfull enough
   # to allow changes in the nix store
   nix.settings.trusted-users = ["@wheel"];
