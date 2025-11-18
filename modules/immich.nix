db85cc219529c454ca9ca0cc8748d2c849af5198
Jan Krejci <krejcijan@protonmail.com>
2025-11-18 10:49:53 +0100

    immich: Ensure the folder structure is available

--- modules/immich.nix
+++ modules/immich.nix
@@ -29,6 +29,22 @@ in {
     # Media stored on dedicated NVMe disk at /var/lib/immich (default)
   };
 
+  # Ensure required directory structure exists on the data disk
+  systemd.tmpfiles.rules = [
+    "d /var/lib/immich/upload 0755 immich immich -"
+    "d /var/lib/immich/library 0755 immich immich -"
+    "d /var/lib/immich/thumbs 0755 immich immich -"
+    "d /var/lib/immich/profile 0755 immich immich -"
+    "d /var/lib/immich/encoded-video 0755 immich immich -"
+    "d /var/lib/immich/backups 0755 immich immich -"
+    "f /var/lib/immich/upload/.immich 0644 immich immich -"
+    "f /var/lib/immich/library/.immich 0644 immich immich -"
+    "f /var/lib/immich/thumbs/.immich 0644 immich immich -"
+    "f /var/lib/immich/profile/.immich 0644 immich immich -"
+    "f /var/lib/immich/encoded-video/.immich 0644 immich immich -"
+    "f /var/lib/immich/backups/.immich 0644 immich immich -"
+  ];
+
   # Nginx reverse proxy - accessible via immich.x.nb
   services.nginx = {
     enable = true;
