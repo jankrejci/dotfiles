5eabf08da35228423c6d836ef7f89226d02e2b0a
Jan Krejci <krejcijan@protonmail.com>
2025-11-19 17:35:30 +0100

    vpsfree: Disable network-online service
    - the configuration is static and handled by vpsAdminOS

--- hosts/vpsfree/configuration.nix
+++ hosts/vpsfree/configuration.nix
@@ -1,4 +1,4 @@
-{...}: {
+{lib, ...}: {
   # Add password for the admin user to be able to log in from local console
   users.users.admin.hashedPassword = "$y$j9T$8qLqeoP/jNv9rFtFfyljl1$S/GqBaFaaCIluY88qW9app4APK49d9wFI.5CmfFnwH/";
 
@@ -8,6 +8,9 @@
 
   # Avoid collision between networking and vpsadminos module
   systemd.network.networks."98-all-ethernet".DHCP = "no";
+
+  # Disable wait-online - network is managed by vpsAdminOS host, not systemd-networkd
+  systemd.services.systemd-networkd-wait-online.enable = lib.mkForce false;
 
   # There is no DHCP, so fixed dns is needed
   networking.nameservers = ["1.1.1.1" "8.8.8.8"];
