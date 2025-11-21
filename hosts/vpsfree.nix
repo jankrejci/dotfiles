12189857b92fbc1ee861194ab3539f4006441a89
Jan Krejci <krejcijan@protonmail.com>
2025-11-21 21:02:33 +0100

    vpsfree: Move the nas specific configuration from backup module

--- hosts/vpsfree.nix
+++ hosts/vpsfree.nix
@@ -1,4 +1,12 @@
-{lib, ...}: {
+{
+  lib,
+  pkgs,
+  ...
+}: let
+  nasHost = "172.16.130.249";
+  nasPath = "/nas/6057";
+  mountPoint = "/mnt/nas-backup";
+in {
   # Add password for the admin user to be able to log in from local console
   users.users.admin.hashedPassword = "$y$j9T$8qLqeoP/jNv9rFtFfyljl1$S/GqBaFaaCIluY88qW9app4APK49d9wFI.5CmfFnwH/";
 
@@ -14,4 +22,40 @@
 
   # There is no DHCP, so fixed dns is needed
   networking.nameservers = ["1.1.1.1" "8.8.8.8"];
+
+  # Backup storage - NFS mount from NAS
+  fileSystems.${mountPoint} = {
+    device = "${nasHost}:${nasPath}";
+    fsType = "nfs";
+    options = ["nofail"];
+  };
+
+  # Bind mount borg repos from NAS
+  fileSystems."/var/lib/borg-repos" = {
+    device = "${mountPoint}/borg-repos";
+    fsType = "none";
+    options = ["bind" "x-systemd.requires=mnt-nas\\x2dbackup.mount"];
+  };
+
+  # Ensure base directory exists on NAS
+  systemd.tmpfiles.rules = [
+    "d ${mountPoint}/borg-repos 0755 borg borg -"
+    "d ${mountPoint}/borg-repos/immich 0700 borg borg -"
+  ];
+
+  # Create dedicated borg user for backup operations
+  users.users.borg = {
+    isSystemUser = true;
+    group = "borg";
+    shell = "${pkgs.bash}/bin/bash";
+    home = "/var/lib/borg";
+    createHome = true;
+  };
+
+  users.groups.borg = {};
+
+  # Install borgbackup for borg serve command
+  environment.systemPackages = with pkgs; [
+    borgbackup
+  ];
 }
