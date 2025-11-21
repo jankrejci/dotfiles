6c6f8b4be87aafc510f08d8c1138889142ed119e
Jan Krejci <krejcijan@protonmail.com>
2025-11-21 23:00:25 +0100

    vpsfree: Disable tmpfs as it is not supported in container environment

--- hosts/vpsfree.nix
+++ hosts/vpsfree.nix
@@ -10,6 +10,9 @@ in {
   # Add password for the admin user to be able to log in from local console
   users.users.admin.hashedPassword = "$y$j9T$8qLqeoP/jNv9rFtFfyljl1$S/GqBaFaaCIluY88qW9app4APK49d9wFI.5CmfFnwH/";
 
+  # vpsAdminOS containers don't support tmpfs on /tmp with systemd mount namespacing
+  boot.tmp.useTmpfs = lib.mkForce false;
+
   systemd.extraConfig = ''
     DefaultTimeoutStartSec=900s
   '';
