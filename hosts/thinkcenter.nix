ce713c477b480e3c802858a6f0f7cfeeb5ada436
jankrejci <krejcijan@protonmail.com>
2025-11-30 15:36:25 +0100

    wireguard: Add tunnel between thinkcenter and vpsfree
    - configure WireGuard tunnel on 10.100.0.0/30 network
    - thinkcenter: 10.100.0.1, vpsfree: 10.100.0.2
    - interface wg0, port 51821 (51820 used by Netbird)
    - endpoint resolution via Netbird DNS (wg.krejci.io)
    - keepalive 25s for NAT traversal
    - keys in /var/lib/wireguard/, public keys in repo
    - tunnel runs over Netbird VPN (no circular dependency)
    - infrastructure ready for potential netbird self-hosting

--- hosts/thinkcenter.nix
+++ hosts/thinkcenter.nix
@@ -1,4 +1,6 @@
-{lib, ...}: {
+{lib, ...}: let
+  vpsfreePublicKey = lib.fileContents ../wireguard-keys/vpsfree-public;
+in {
   # Add password for the admin user to be able to log in from local console
   users.users.admin.hashedPassword = "$y$j9T$8qLqeoP/jNv9rFtFfyljl1$S/GqBaFaaCIluY88qW9app4APK49d9wFI.5CmfFnwH/";
 
@@ -6,4 +8,45 @@
 
   # Enable processes collector for detailed monitoring (systemd already enabled in common.nix)
   services.prometheus.exporters.node.enabledCollectors = lib.mkForce ["systemd" "processes"];
+
+  # WireGuard exporter for wg0 tunnel monitoring
+  services.prometheus.exporters.wireguard = {
+    enable = true;
+    port = 9586;
+    openFirewall = false;
+    latestHandshakeDelay = true;
+    verbose = false;
+  };
+
+  # Allow WireGuard exporter on Netbird interface only
+  networking.firewall.interfaces."nb-homelab".allowedTCPPorts = [9586];
+
+  # WireGuard tunnel to vpsfree for NetBird self-hosted
+  # thinkcenter (behind NAT) -> vpsfree (public gateway)
+  networking.firewall.allowedUDPPorts = [51821];
+
+  systemd.network.netdevs."50-wg0" = {
+    netdevConfig = {
+      Kind = "wireguard";
+      Name = "wg0";
+    };
+    wireguardConfig = {
+      PrivateKeyFile = "/var/lib/wireguard/wg-thinkcenter-private";
+      ListenPort = 51821;
+    };
+    wireguardPeers = [
+      {
+        PublicKey = vpsfreePublicKey;
+        AllowedIPs = ["10.100.0.2/32"];
+        Endpoint = "wg.krejci.io:51821";
+        PersistentKeepalive = 25;
+      }
+    ];
+  };
+
+  systemd.network.networks."50-wg0" = {
+    matchConfig.Name = "wg0";
+    address = ["10.100.0.1/30"];
+    networkConfig.DHCP = "no";
+  };
 }
