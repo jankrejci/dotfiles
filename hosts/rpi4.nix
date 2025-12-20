7384cb14417f5b691e4bb53e1ebca8939eedb6d2
jankrejci <krejcijan@protonmail.com>
2025-12-20 10:16:16 +0100

    rpi4: Add host down alert

--- hosts/rpi4.nix
+++ hosts/rpi4.nix
@@ -1,3 +1,22 @@
-{inputs, ...}: {
-  imports = [inputs.nixos-raspberrypi.nixosModules.raspberry-pi-4.base];
+{
+  config,
+  inputs,
+  ...
+}: {
+  imports = [
+    inputs.nixos-raspberrypi.nixosModules.raspberry-pi-4.base
+  ];
+
+  homelab.alerts.hosts = [
+    {
+      alert = "Rpi4Down";
+      expr = ''up{instance=~"rpi4.*", job="node"} == 0'';
+      labels = {
+        severity = "critical";
+        host = config.homelab.host.hostName;
+        type = "host";
+      };
+      annotations.summary = "rpi4 host is down";
+    }
+  ];
 }
